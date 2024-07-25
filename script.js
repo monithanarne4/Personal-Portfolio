62ef9ba1a564095a0802e22db21f00b5a782fe3f
monithanarne4 <monitha_narne@srmap.edu.in>
2024-07-25 23:35:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Front-End Developer" , "Content Creater" , "Photographer" ,"Video Editer"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+  });
